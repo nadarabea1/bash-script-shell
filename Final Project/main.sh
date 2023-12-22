7e6cd04b0376ff770e774bb5bea5c15f6b6f611a
nadarabea1 <nadarabea81@gmail.com>
2023-12-22 15:04:35 +0200

    Edit create table function $ some retouches

--- Final Project/main.sh
+++ Final Project/main.sh
@@ -4,16 +4,16 @@ dir="/home/nadarabea/FinalProject"
 
 function DropTable() {
         read -p "Enter the Name of Table : " tab
-        if [ -f $dir/$conn/$tab ]
+        if [ -d $dir/$conn/$tab ]
         then
                 read -p "Are you Sure? (Y | N)" ans
-                if [ $ans=='Y' ] || [ $ans=='y' ]
+                if [ $ans == 'Y' ] || [ $ans == 'y' ]
                 then
                         rm $dir/$conn/$tab
-                        echo Table was Dropped
+                        echo "Table '$tab' dropped successfully."
                 fi
         else
-                echo NO Such Table
+                echo "Not found '$tab' table"
         fi
 }
 
@@ -22,43 +22,25 @@ function ListTable() {
 }
 
 function CreateTable() {
-        read -p "Enter Table Name: " tab
-        declare -a "$tab"=""
-        if [ -f $dir/$conn/$tab ]
-        then
-                echo "There is Table with the same name ;( "
-        else
-                touch $dir/$conn/$tab
-                echo "Table Created ;) "        
+	read -p "Enter the table name: " tab
+	if [  -d $dir/$conn/$tab ]
+	then
+		echo Table $tab exists
+	else
+        	read -p "Enter the columns (comma-separated, e.g., col1,col2,col3): " cols
+		read -p "Enter the data types for each column (comma-separated, e.g., int,string): " datatypes
+		read -p "Enter the primary key: "  pk
+		mkdir $dir/$conn/$tab 
+		touch $dir/$conn/$tab/schema
+		touch $dir/$conn/$tab/data
+		echo "Columns:$cols" > "$dir/$conn/$tab/schema"
+		echo "Datatypes:$datatypes" >> "$dir/$conn/$tab/schema"
+		echo "Primarykey:$pk" >> "$dir/$conn/$tab/schema"
 
-
-
-
-                read -p "Enter the number of colums : " cols
-                printf '#' > $dir/$conn/$tab
-                ans='n'
-                for ((i=1; i<=$cols; i++))
-                do
-                        read -p "Enter Name of Colum $i: " name
-                        colArr+=("$name")
-                        read -p "Enter Data type(int | string | bool): " dtype
-                        if [ $ans == 'n' ] || [ $ans == 'N' ]
-                        then
-                                read -p "Is primary key (Y|N)? " ans
-                                if [ $ans == 'Y' ] || [ $ans == 'y' ]
-                                then
-                                        name=$name*
-                                fi
-                        fi
-
-                        printf $name-$dtype: >> $dir/$conn/$tab
-                done
-
-                printf '\n' >> $dir/$conn/$tab
-
-        fi
-
+		echo "Table '$tab' created successfully."
+	fi
 }
+
 function List() {
         ls -R $dir
 }
@@ -69,6 +51,7 @@ function CreateDB() {
                 read -p "Enter another name of DB: " crt
         else
                 mkdir $dir/$crt
+                echo "DB '$crt' created successfully"
         fi
 
 }
@@ -80,11 +63,11 @@ function Drop() {
                 if [ $ans == "Y" ] || [ $ans == "y" ]
                 then
                         rm -R $dir/$drop
-                        echo "Drop $drop DB"
+                        echo "DB '$drop' dropped successfully."                
                 fi
 
         else
-                echo NO Found DB!
+                echo Not Found DB!
         fi
 }
 
